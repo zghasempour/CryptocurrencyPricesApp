2aaa3c2201835bdb7f75d72b54c0d8e4bb61313a
zghasempour <ghasempour.zahra0@gmail.com>
2022-06-26 22:14:01 +0430

    add mpAndroid chart

--- app/src/main/java/com/example/cryptocurrencypricesapp/network/Data.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cryptocurrencypricesapp.network
-
-import com.tradingview.lightweightcharts.api.series.common.SeriesData
-import com.tradingview.lightweightcharts.api.series.enums.SeriesType
-
-data class Data(
-    val list: List<SeriesData>,
-    val type: SeriesType
-)
